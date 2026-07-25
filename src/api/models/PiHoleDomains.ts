45f216961da78f9b470b899f7b5f22c537bdbbda
HyperCriSiS <dev.github@w3b.world>
2026-07-25 22:42:52 +0200

    Expose reusable Pi-hole domain model

--- src/api/models/PiHoleDomains.ts
+++ src/api/models/PiHoleDomains.ts
@@ -1,21 +1,26 @@
+export type PiHoleDomain = {
+  domain: string
+  unicode: string
+  type: string
+  kind: string
+  comment: string | null
+  groups: number[]
+  enabled: boolean
+  id: number
+  date_added: number
+  date_modified: number
+}
+
 export type PiHoleDomains = {
-  domains: Array<{
-    domain: string
-    unicode: string
-    type: string
-    kind: string
-    comment: string
-    groups: Array<number>
-    enabled: boolean
-    id: number
-    date_added: number
-    date_modified: number
-  }>
-  processed: {
-    errors: Array<any>
+  domains: PiHoleDomain[]
+  processed?: {
+    errors: Array<{
+      item: string
+      error?: string
+    }>
     success: Array<{
       item: string
     }>
-  }
+  } | null
   took: number
 }
