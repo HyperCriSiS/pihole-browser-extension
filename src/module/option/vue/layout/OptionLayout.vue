e8f7594d2580f26b24f4a31380a40489095b80b1
HyperCriSiS <dev.github@w3b.world>
2026-07-26 03:22:35 +0200

    Migrate the options layout to Vuetify 4

--- src/module/option/vue/layout/OptionLayout.vue
+++ src/module/option/vue/layout/OptionLayout.vue
@@ -1,74 +1,48 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img height="60px" src="icon/icon-128.png" contain />
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            PiHole
-            <br />Browser <br />Extension
-          </v-list-item-title>
-        </v-list-item-content>
+    <v-navigation-drawer>
+      <v-list-item class="py-3">
+        <template #prepend>
+          <v-avatar size="60" image="icon/icon-128.png" />
+        </template>
+        <v-list-item-title class="text-h6">
+          PiHole<br />Browser<br />Extension
+        </v-list-item-title>
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list dense nav>
-        <v-list-item link to="/">
-          <v-list-item-icon>
-            <v-icon>{{ mdiCog }}</v-icon>
-            ️
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title
-              >{{ translate(I18NOptionKeys.options_settings) }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link to="/about">
-          <v-list-item-icon>
-            <v-icon>{{ mdiInformationOutline }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title
-              >{{ translate(I18NOptionKeys.options_about) }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      <v-list density="compact" nav>
+        <v-list-item
+          link
+          to="/"
+          :prepend-icon="mdiCog"
+          :title="translate(I18NOptionKeys.options_settings)"
+        />
+        <v-list-item
+          link
+          to="/about"
+          :prepend-icon="mdiInformationOutline"
+          :title="translate(I18NOptionKeys.options_about)"
+        />
         <v-divider />
         <v-list-item
           link
           :href="LinkConfig.paypal_donation_link"
           target="_blank"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ mdiGift }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title
-              >{{ translate(I18NOptionKeys.option_donation) }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link :href="LinkConfig.github_issue" target="_blank">
-          <v-list-item-icon>
-            <v-icon>{{ mdiFire }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title
-              >{{ translate(I18NOptionKeys.option_troubleshooting) }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+          :prepend-icon="mdiGift"
+          :title="translate(I18NOptionKeys.option_donation)"
+        />
+        <v-list-item
+          link
+          :href="LinkConfig.github_issue"
+          target="_blank"
+          :prepend-icon="mdiFire"
+          :title="translate(I18NOptionKeys.option_troubleshooting)"
+        />
       </v-list>
       <template #append>
-        <v-alert color="primary" outlined class="mx-5">
+        <v-alert color="primary" variant="outlined" class="mx-5">
           {{ copyrightText }}
         </v-alert>
       </template>
