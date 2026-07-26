ffe834a9778bbdfecfffa35d16d5ba864eed259e
HyperCriSiS <dev.github@w3b.world>
2026-07-26 04:54:47 +0200

    Register Vuetify components and directives

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,6 @@
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
@@ -16,6 +18,8 @@ const colors = {
 }
 
 const vuetify = createVuetify({
+  components,
+  directives,
   icons: {
     defaultSet: 'mdi',
     aliases,
