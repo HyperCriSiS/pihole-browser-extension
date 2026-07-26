1d7fd9b7cfccb22734cf51eabcff1d7c568b6933
HyperCriSiS <dev.github@w3b.world>
2026-07-26 02:16:26 +0200

    Add ESLint flat configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,78 @@
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**', 'package.*.zip']
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/vue2-recommended'],
+  {
+    files: ['src/**/*.{ts,vue}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ['.vue']
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'class-methods-use-this': 'off',
+      'no-restricted-syntax': ['off', 'ForOfStatement'],
+      'prefer-promise-reject-errors': 'off',
+      'no-plusplus': 'off',
+      'no-console': 'off'
+    }
+  },
+  {
+    files: ['src/**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ['.vue']
+      }
+    }
+  },
+  {
+    files: ['src/webpack/**/*.ts', '*.js', '*.mjs'],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: [
+      'src/module/popup/vue/components/PopupListCardComponent.vue',
+      'src/module/popup/vue/components/PopupStatusCardComponent.vue'
+    ],
+    rules: {
+      'prefer-destructuring': 'off'
+    }
+  },
+  {
+    files: ['src/service/PiHoleApiService.ts'],
+    rules: {
+      'consistent-return': 'off'
+    }
+  },
+  {
+    files: ['src/service/TemporaryActionService.ts'],
+    rules: {
+      'no-await-in-loop': 'off',
+      'no-continue': 'off'
+    }
+  },
+  eslintConfigPrettier
+)
